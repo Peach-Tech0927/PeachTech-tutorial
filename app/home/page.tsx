590affb8d09adf488365dcc88ad59e91ad744bc0
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-05-16 00:22:50 +0900

    背景色変更

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Card1 from "../components/card";
+import { Box, Typography } from "@mui/material";
 
 const page = () => {
   return (
-    <div>
+    <Box bgcolor={"#fff7f7"} height={900}>
       <Card1 />
-    </div>
+      <Typography>最後まで終わった方はこちら！</Typography>
+    </Box>
   );
 };
 
