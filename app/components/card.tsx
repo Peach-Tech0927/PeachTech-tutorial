bc76fce3594ed78d8ae47191773b367247a5886a
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-05-16 20:35:55 +0900

    カード一枚作ってみた

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,24 +1,32 @@
 import {
   Card,
   CardActionArea,
-  CardMedia,
   CardContent,
   Typography,
+  Box,
 } from "@mui/material";
 import Image from "next/image";
+import Healthicons1Outline from "../components/icons";
 
 const Card1 = () => {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
-        <Typography fontSize={20} component="div">
-          この動画を見よう！
-        </Typography>
-        <Image src="/sample.png" width={300} height={200} alt="サンプル画像" />
+        <Box  display={"flex"}>
+          <Healthicons1Outline />
+          <Typography fontSize={20} component="div" marginTop={1.5} >
+            この動画を見よう！
+          </Typography>
+        </Box>
+        <Box marginLeft={3}>
+        <Image src="/sample.png" width={280} height={180} alt="サンプル画像" />
+        </Box>
         <CardContent></CardContent>
       </CardActionArea>
     </Card>
   );
 };
+
+
 
 export default Card1;
