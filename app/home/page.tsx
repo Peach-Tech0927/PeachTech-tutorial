d7846f5d472229dca3ba28b1237bb0db02a2e0fd
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-05-16 20:37:27 +0900

    pageにshowcard追加

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Card1 from "../components/card";
-import { Box, Typography } from "@mui/material";
+ import ShowCard from "../components/showcard";
+
+import { Box, Typography, Icon, Button } from "@mui/material";
+
 
 const page = () => {
   return (
-    <Box bgcolor={"#fff7f7"} height={900}>
-      <Card1 />
-      <Typography>最後まで終わった方はこちら！</Typography>
+    <Box bgcolor={"#fff7f7"} height={900} position={"relative"}>
+      <Box position={"absolute"} margin={5}> 
+        {/* ここでmarginとると背景色が適応されない部分が出来てしまう */}
+        <ShowCard />
+      </Box>
+      <Box position={"absolute"} right={0} bottom={0}>
+        <Button>
+          <Typography color={"#798EFE"}>
+            最後まで終わった方はこちら！
+          </Typography>
+        </Button>
+      </Box>
     </Box>
   );
 };
