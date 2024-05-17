b18113115d432db29a9892cd082a01518b19f4af
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-05-17 19:18:09 +0900

    thumbnail-card.tsx,thumbnail-card-area.tsxの作成

---
+++ app/components/thumbnail-card-area.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import { cardData } from "../const/card-data";
+import ThumbnailCard from "./thumbnail-card";
+import { Box } from "@mui/material";
+
+const ThumbnailCardArea = () => {
+  return (
+      <Box px={{ xs: 2, md: 15 }} bgcolor={"#fff7f7"}>
+        <Grid container spacing={4} padding={3}>
+          {cardData.map((data) => (
+            <Grid
+              item
+              xs={12}
+              sm={4}
+              md={4}
+              key={data.number}
+              display={"flex"}
+              justifyContent={"center"}
+              alignContent={"center"}
+            >
+              <ThumbnailCard
+                title={data.title}
+                imageSrc={data.imageSrc}
+                number={data.number}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+  );
+};
+
+export default ThumbnailCardArea;
