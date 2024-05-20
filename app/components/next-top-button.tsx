be32f69c441f3b88d0dcad8edfba0c6d5b318965
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-05-20 23:23:44 +0900

    プログラム見やすくした

--- app/components/next-top-button.tsx
+++ app/components/next-top-button.tsx
@@ -2,10 +2,13 @@ import { Box, Button, Typography } from "@mui/material";
 
 const NextTopbutton = () => {
   return (
-     <Box display="flex">
-       <Button sx={{ color: "#5f82ff"  , ml:{xs:1, md:3}}}> ＜ Topへ戻る</Button>
-       <Box margin="0 0 0 auto">
-        <Button variant="contained" sx={{ bgcolor: "#5f82ff" , mr:{xs:1,md:3}, borderRadius: 2}}>
+    <Box display="flex">
+      <Button sx={{ color: "#5f82ff", ml: { xs: 1, md: 3 } }}>Topへ戻る</Button>
+      <Box margin="0 0 0 auto">
+        <Button
+          variant="contained"
+          sx={{ bgcolor: "#5f82ff", mr: { xs: 1, md: 3 }, borderRadius: 2 }}
+        >
           次へ
         </Button>
       </Box>
