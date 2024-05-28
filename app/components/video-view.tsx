72022fc757024d3bb5e6b507f70e99b167102be8
momo-41 <minimizu41@gmail.com>
2024-05-28 19:54:14 +0900

    動画説明のページにおける、サムネイルと概要の部分をコンポーネント化した

--- app/components/video-view.tsx
+++ app/components/video-view.tsx
@@ -1,8 +1,6 @@
-import { Box, Button, Typography } from "@mui/material";
-import Image from "next/image";
+import { Box, Typography } from "@mui/material";
 import { VideoPageProps } from "../const/video-view-data";
-import TextWithImageArea from "./text-with-image-area";
-import { TextWithImageData } from "../const/text-with-image-data";
+import VideoArea from "./video-area";
 
 const VideoView: React.FC<VideoPageProps> = ({
   step,
@@ -42,74 +40,7 @@ const VideoView: React.FC<VideoPageProps> = ({
           <br />
           見終わったら右下の次へボタンを押して次に進んでください！
         </Typography>
-        {TextWithImageData.map((data) => (
-          <TextWithImageArea
-            title={data.title}
-            detail={data.detail}
-            img={data.img}
-          />
-        ))}
-        <Box
-          display={"flex"}
-          alignItems={"center"}
-          justifyContent={"center"}
-          flexDirection={"column"}
-        >
-          <Box
-            position={"relative"}
-            display={"flex"}
-            justifyContent={"center"}
-            mx={3}
-          >
-            <Image
-              src={img}
-              alt={"thumbnail-1"}
-              width={500}
-              height={300}
-              style={{
-                maxWidth: "100%",
-                height: "auto",
-              }}
-            />
-            <Button
-              variant="contained"
-              sx={{
-                color: "white",
-                bgcolor: "#5071E5",
-                "&:hover": {
-                  background: "#3758CD",
-                },
-                px: { xs: 3, md: 4 },
-                py: 1,
-                fontSize: { xs: 14, md: 17 },
-                position: "absolute",
-                bottom: { xs: 22, md: 40 },
-                zIndex: 1,
-              }}
-              href={href}
-            >
-              動画を見る
-            </Button>
-          </Box>
-          <Box
-            bgcolor={"#f2f1f1"}
-            mb={{ xs: 10, md: 20 }}
-            mt={{ xs: 4, md: 7 }}
-            px={{ xs: 4, md: 8 }}
-            py={{ xs: 1.5, md: 2 }}
-          >
-            <Typography
-              fontSize={{ xs: 13, md: 16 }}
-              fontWeight={"bold"}
-              mb={{ xs: 0.5, md: 1 }}
-            >
-              概要
-            </Typography>
-            {overView.map((data) => (
-              <Typography fontSize={{ xs: 12, md: 17 }}>・{data}</Typography>
-            ))}
-          </Box>
-        </Box>
+        <VideoArea img={img} overView={overView} href={href} />
         {/* <Button
           variant="contained"
           sx={{ position: "absolute", right: 30, bottom: 30 }}
