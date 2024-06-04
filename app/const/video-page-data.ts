0e6325d153a98f198b8d8475ef06e26819893755
momo-41 <minimizu41@gmail.com>
2024-06-04 16:16:45 +0900

    section2,3,4のデータの作成とページの作成

--- app/const/video-page-data.ts
+++ app/const/video-page-data.ts
@@ -42,3 +42,37 @@ export const step2VideoPageData: VideoPageProps = {
   buttonText: "次へ",
   nextButtonHref: "/section3",
 };
+
+export const step3VideoPageData: VideoPageProps = {
+  step: 3,
+  title: "Discordの使い方の動画を見よう！",
+  detail:
+    "Discordの使い方の説明です！！PeachTechにはたくさんのチャンネルがあります！その紹介などもしているのでDiscordを使ったことある人も全部見てください！！",
+  img: "/discord-screenshot.png",
+  contents: [
+    "timesの説明について",
+    "メンターについて",
+    "もくもく部屋の紹介",
+    "自己紹介",
+  ],
+  href: "/",
+  buttonText: "次へ",
+  nextButtonHref: "/section4",
+};
+
+export const step4VideoPageData: VideoPageProps = {
+  step: 4,
+  title: "Discordの使い方の動画を見よう！",
+  detail:
+    "Discordの使い方の説明です！！PeachTechにはたくさんのチャンネルがあります！その紹介などもしているのでDiscordを使ったことある人も全部見てください！！",
+  img: "/discord-screenshot.png",
+  contents: [
+    "timesの説明について",
+    "メンターについて",
+    "もくもく部屋の紹介",
+    "自己紹介",
+  ],
+  href: "/",
+  buttonText: "次へ",
+  nextButtonHref: "/section5",
+};
