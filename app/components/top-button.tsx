8e4a28afbac493c0804d50038423a58f2a0f194b
momo-41 <minimizu41@gmail.com>
2024-06-04 16:43:51 +0900

    次へボタンとTOPへ戻るボタンのレスポンシブ対応

--- app/components/top-button.tsx
+++ app/components/top-button.tsx
@@ -2,7 +2,7 @@ import { Button } from "@mui/material";
 
 const TopButton = () => {
   return (
-    <Button href="/" sx={{ color: "#5f82ff", fontSize: 16 }}>
+    <Button href="/" sx={{ color: "#5f82ff", fontSize: { xs: 12, md: 16 } }}>
       ＜ Topへ戻る
     </Button>
   );
