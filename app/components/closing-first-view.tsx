9b0e3027d5e52f1166aef8a6613836795b2aac63
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-06-05 11:07:23 +0900

    不要なコード削除　コード見やすくした

--- app/components/closing-first-view.tsx
+++ app/components/closing-first-view.tsx
@@ -2,50 +2,44 @@ import { Typography, Box } from "@mui/material";
 
 const ClosingFirstView = () => {
   return (
-    <>
+    <Box
+      position="relative"
+      height={{ xs: "45vh", md: "75vh" }}
+      sx={{
+        backgroundImage: `url("/closing-first.png")`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
       <Box
-        position="relative"
-        height={{ xs: "45vh", md: "75vh" }}
+        height={"100%"}
         sx={{
-          backgroundImage: `url("/closing-first.png")`,
-          backgroundPosition: "center",
-          backgroundSize: "cover",
+          backdropFilter: "blur(3px)",
+          backgroundColor: "rgba(255,255,255,0.5)",
         }}
       >
         <Box
-          height={"100%"}
-          sx={{
-            backdropFilter: "blur(3px)",
-            backgroundColor: "rgba(255,255,255,0.5)",
-          }}
+          display="flex"
+          alignItems="center"
+          pt={{ xs: 10, md: 20 }}
+          flexDirection="column"
         >
-          <Box
-            display="flex"
-            width={"100%"}
-            justifyContent="center"
-            alignItems="center"
-            pt={{ xs: 10, md: 20 }}
-            sx={{
-              flexFlow: "column",
-            }}
+          <Typography fontSize={{ xs: 15, md: 25 }} mb={2}>
+            動画視聴お疲れ様でした！！
+          </Typography>
+          <Typography
+            m="0 auto"
+            fontSize={{ xs: 15, md: 25 }}
+            mb={{ xs: 7.5, md: 15 }}
           >
-            <Typography fontSize={{ xs: 15, md: 25 }} mb={2}>
-              動画視聴お疲れ様でした！！
-            </Typography>
-            <Typography
-              m="0 auto"
-              fontSize={{ xs: 15, md: 25 }}
-              mb={{ xs: 7.5, md: 15 }}
-            >
-              PeachTechチュートリアルはこれで完了です！
-            </Typography>
-            <Typography m="0 auto" fontSize={{ xs: 15, md: 25 }}>
-              これから一緒に頑張りましょう！！
-            </Typography>
-          </Box>
+            PeachTechチュートリアルはこれで完了です！
+          </Typography>
+          <Typography m="0 auto" fontSize={{ xs: 15, md: 25 }}>
+            これから一緒に頑張りましょう！！
+          </Typography>
         </Box>
       </Box>
-    </>
+    </Box>
   );
 };
 
