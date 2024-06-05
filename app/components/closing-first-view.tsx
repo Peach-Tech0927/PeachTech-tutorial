a29e88515956907d8076c17fc14193b2fa9406b6
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-06-05 18:12:22 +0900

    コードきれいにした

--- app/components/closing-first-view.tsx
+++ app/components/closing-first-view.tsx
@@ -21,20 +21,17 @@ const ClosingFirstView = () => {
         <Box
           display="flex"
           alignItems="center"
+          justifyContent="center"
           pt={{ xs: 10, md: 20 }}
           flexDirection="column"
         >
           <Typography fontSize={{ xs: 15, md: 25 }} mb={2}>
             動画視聴お疲れ様でした！！
           </Typography>
-          <Typography
-            m="0 auto"
-            fontSize={{ xs: 15, md: 25 }}
-            mb={{ xs: 7.5, md: 15 }}
-          >
+          <Typography fontSize={{ xs: 15, md: 25 }} mb={{ xs: 7.5, md: 15 }}>
             PeachTechチュートリアルはこれで完了です！
           </Typography>
-          <Typography m="0 auto" fontSize={{ xs: 15, md: 25 }}>
+          <Typography fontSize={{ xs: 15, md: 25 }}>
             これから一緒に頑張りましょう！！
           </Typography>
         </Box>
