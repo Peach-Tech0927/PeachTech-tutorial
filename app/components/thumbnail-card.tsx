5645fcc11b46b61c26499c653ecaf680090d0a74
momo-41 <minimizu41@gmail.com>
2024-06-07 19:18:03 +0900

    サムネイルカードのhrefをCardActionAreaからnext/linkに変更した

--- app/components/thumbnail-card.tsx
+++ app/components/thumbnail-card.tsx
@@ -1,6 +1,7 @@
 import { Card, CardActionArea, Typography, Box } from "@mui/material";
 import Image from "next/image";
 import { cardDataProps } from "../types/types";
+import Link from "next/link";
 
 const ThumbnailCard: React.FC<cardDataProps> = ({
   title,
@@ -9,19 +10,21 @@ const ThumbnailCard: React.FC<cardDataProps> = ({
   href,
 }) => {
   return (
-    <Card sx={{ maxWidth: "auto", borderRadius: "20px" }}>
-      <CardActionArea sx={{ px: 1 }} href={href}>
-        <Box display={"flex"} py={2}>
-          <Image src={number} width={45} height={45} alt="サンプル画像" />
-          <Typography fontSize={{ xs: 23, md: 20 }} marginTop={1.5}>
-            {title}
-          </Typography>
-        </Box>
-        <Box pb={2} px={2}>
-          <Image src={imageSrc} width={250} height={160} alt="サンプル画像" />
-        </Box>
-      </CardActionArea>
-    </Card>
+    <Link href={href} style={{ textDecoration: "none" }}>
+      <Card sx={{ maxWidth: "auto", borderRadius: "20px" }}>
+        <CardActionArea sx={{ px: 1 }}>
+          <Box display={"flex"} py={2}>
+            <Image src={number} width={45} height={45} alt="サンプル画像" />
+            <Typography fontSize={{ xs: 23, md: 20 }} marginTop={1.5}>
+              {title}
+            </Typography>
+          </Box>
+          <Box pb={2} px={2}>
+            <Image src={imageSrc} width={250} height={160} alt="サンプル画像" />
+          </Box>
+        </CardActionArea>
+      </Card>
+    </Link>
   );
 };
 
