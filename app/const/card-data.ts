9001ed71516432a072ee66d1c07bc0dd15cd1310
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-06-14 14:17:42 +0900

    サムネイル画像の挿入

--- app/const/card-data.ts
+++ app/const/card-data.ts
@@ -2,39 +2,39 @@ import { cardDataProps } from "../types/types";
 
 export const cardData: cardDataProps[] = [
   {
-    title: "Discordの使い方の動画を見よう！",
-    imageSrc: "/sample.png",
-    number: "/number1.png",
+    title: "Discordの説明",
+    imageSrc: "/discord-image.png",
+    number: "1",
     href: "/section1",
   },
   {
     title: "メンバーズサイトについて",
-    imageSrc: "/sample.png",
-    number: "/number2.png",
+    imageSrc: "/memberssite-image.png",
+    number: "2",
     href: "/section2",
   },
   {
     title: "TechTrainの紹介",
-    imageSrc: "/sample.png",
-    number: "/number3.png",
+    imageSrc: "/TechTrain-image.png",
+    number: "3",
     href: "/section3",
   },
   {
-    title: "未定",
-    imageSrc: "/sample.png",
-    number: "/number4.png",
+    title: "色々な設定",
+    imageSrc: "/setting-image.png",
+    number: "4",
     href: "/section4",
   },
   {
     title: "オンラインにしよう！",
-    imageSrc: "/sample.png",
-    number: "/number5.png",
+    imageSrc: "/online-image.png",
+    number: "5",
     href: "/section5",
   },
   {
     title: "メンションと返信について",
-    imageSrc: "/sample.png",
-    number: "/number6.png",
+    imageSrc: "/reply-image.png",
+    number: "6",
     href: "/section6",
   },
 ];
