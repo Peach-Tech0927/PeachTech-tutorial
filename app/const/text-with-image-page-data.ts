ec8d3f56c9570ecf6d8676a989198838427e04ae
momo-41 <minimizu41@gmail.com>
2024-06-16 23:01:03 +0900

    video-pageとtext-with-image-pageのtitleとdetailとimgを追加

--- app/const/text-with-image-page-data.ts
+++ app/const/text-with-image-page-data.ts
@@ -2,56 +2,46 @@ import { TextWithImagePageProps } from "../types/types";
 
 export const step5TextWithImagePageData: TextWithImagePageProps = {
   step: 5,
-  title: "Discordでオンラインが表示されるようにしよう！",
+  title: "Discordをオンラインにしよう！",
   detail:
-    "Discordでは誰が今オンラインになっているかが分かります！自分がdiscordを開いた時にオンラインのマークが出るように設定します！",
+    "Discordでは誰がオンラインになっているかが分かります！このStepでは自分がDiscordを開いた時にオンラインのマークが出るように設定してほしいです！その表示をすることでメンバーが話しかけやすくなったり、認知されやすくなります！！",
   textWithImageArea: [
     {
-      title: "1.自分のアイコンをクリック",
-      detail: "以下の画像の画面で、左下にある自分のアイコンをクリック！",
-      img: "/discord-screenshot.png",
+      title: "1. オンラインのメンバーリストを表示",
+      detail:
+        "Discordでは以下の画像のように右サイドバーにメンバーリストが表示され、オンラインかどうかが分かります！このサイドバーがない人は、以下の画像で示している右上の赤い印をクリックすると表示されます！この時点で自分が緑色のオンラインマーク付きで表示されている場合は、次のStep6へ進んで大丈夫です！",
+      img: "/text-with-image-5-1.jpeg",
     },
     {
-      title: "2.オンラインを選択",
+      title: "2. ステータスをオンラインにする",
       detail:
-        "以下の画像の印のところにカーソルを合わせると色々なオンラインステータスが表示されます。こちらでオンラインを選択してください🙌",
-      img: "/discord-screenshot.png",
-    },
-    {
-      title: "3.オンラインの確認",
-      detail:
-        "以下の画像の左側のサイドバーに、自分がオンラインで表示されているか確認してみてください！もしそのサイドバーが無い場合は、その上にある人型のアイコンをクリックすると現れます！",
-      img: "/discord-screenshot.png",
+        "以下の画像で示している、自分のアイコンをクリック！その時に表示されるところで以下の画像のように「オンライン」を選択してください！右のサイドバーで自分が緑色のオンラインマークで表示されていれば大丈夫です！！",
+      img: "/text-with-image-5-2.jpeg",
     },
   ],
-  buttonText: "完了！",
-  nextButtonHref: "/closing",
+  buttonText: "次へ",
+  nextButtonHref: "/section6",
 };
 
 export const step6TextWithImagePageData: TextWithImagePageProps = {
   step: 6,
-  title: "メンションや返信について",
+  title: "Discordで話すときはメンションか返信をしよう！",
   detail:
-    "Discordでは誰が今オンラインになっているかが分かります！自分がdiscordを開いた時にオンラインのマークが出るように設定します！",
+    "Discordで会話をするときは必ずメンションか返信機能を使ってほしいです！そうすることで相手に通知がいきます！メンションや返信機能を使わないと通知がいかないのでメッセージに気づいてもらえないかもしれません！またStep4でもあったように、質問するときもぜひメンションをしてみてください！🙌 ",
   textWithImageArea: [
     {
-      title: "1.自分のアイコンをクリック",
-      detail: "以下の画像の画面で、左下にある自分のアイコンをクリック！",
-      img: "/discord-screenshot.png",
+      title: "1. メンションの仕方",
+      detail:
+        "半角の「@」を入力してメンションをしたいメンバーの名前を入力するとメンションができます！そのメンションの文字が青色になっていればOKです！！",
+      img: "/text-with-image-6-1.jpeg",
     },
     {
-      title: "2.オンラインを選択",
+      title: "2. 返信の仕方",
       detail:
-        "以下の画像の印のところにカーソルを合わせると色々なオンラインステータスが表示されます。こちらでオンラインを選択してください🙌",
-      img: "/discord-screenshot.png",
-    },
-    {
-      title: "3.オンラインの確認",
-      detail:
-        "以下の画像の左側のサイドバーに、自分がオンラインで表示されているか確認してみてください！もしそのサイドバーが無い場合は、その上にある人型のアイコンをクリックすると現れます！",
-      img: "/discord-screenshot.png",
+        "返信をしたい人のメッセージにカーソルを当てると、以下の画像のように曲がった矢印マークが出てくるのでそれをクリックします！そのままメッセージを入力して送信すると、相手に通知が届いて返信ができます！また返信をしたい人のメッセージを右クリックしても「返信」が出てきます！",
+      img: "/text-with-image-6-2.jpeg",
     },
   ],
   buttonText: "完了！",
-  nextButtonHref: "/final",
+  nextButtonHref: "/closing",
 };
