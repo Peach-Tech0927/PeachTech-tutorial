c848f3db200c7e6d52af4e404d42d4ebb821a68d
momo-41 <minimizu41@gmail.com>
2024-06-18 01:03:12 +0900

    text-with-imageの画像をクリックした時にモーダルで表示させるように実装(スマホではモーダルさせないようにしている)

--- app/components/text-with-image-area.tsx
+++ app/components/text-with-image-area.tsx
@@ -1,12 +1,27 @@
-import { Box, Typography } from "@mui/material";
+"use client";
+import * as React from "react";
+import { Box, Button, Modal, Typography, useMediaQuery } from "@mui/material";
 import Image from "next/image";
 import { TextWithImageAreaProps } from "../types/types";
+
+const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: { xs: 600, md: 1000 },
+};
 
 const TextWithImageArea: React.FC<TextWithImageAreaProps> = ({
   title,
   detail,
   img,
 }) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const isPC = useMediaQuery("(min-width:600px)");
+
   return (
     <Box px={1} mt={7} mb={{ xs: 10, md: 14 }}>
       <Typography fontWeight={"bold"} fontSize={{ xs: 15, md: 19 }}>
@@ -16,16 +31,45 @@ const TextWithImageArea: React.FC<TextWithImageAreaProps> = ({
         {detail}
       </Typography>
       <Box mx={{ xs: 0, md: 1 }} mt={{ xs: 1, md: 3 }}>
-        <Image
-          width={580}
-          height={350}
-          src={img}
-          alt={"description-img"}
-          style={{
-            maxWidth: "100%",
-            height: "auto",
-          }}
-        />
+        {isPC ? (
+          <Button onClick={handleOpen}>
+            <Image
+              width={580}
+              height={350}
+              src={img}
+              alt={"description-img"}
+              style={{
+                maxWidth: "100%",
+                height: "auto",
+              }}
+            />
+          </Button>
+        ) : (
+          <Image
+            width={580}
+            height={350}
+            src={img}
+            alt={"description-img"}
+            style={{
+              maxWidth: "100%",
+              height: "auto",
+            }}
+          />
+        )}
+        <Modal open={open} onClose={handleClose} sx={{ outline: "none" }}>
+          <Box sx={{ ...modalStyle }}>
+            <Image
+              width={1000}
+              height={350}
+              src={img}
+              alt={"description-img"}
+              style={{
+                maxWidth: "100%",
+                height: "auto",
+              }}
+            />
+          </Box>
+        </Modal>
       </Box>
     </Box>
   );
