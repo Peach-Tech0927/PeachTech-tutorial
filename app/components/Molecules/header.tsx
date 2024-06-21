e4543342815c7a2067a648ef9fa7898de2aa0a05
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-06-21 20:05:30 +0900

    headerのpeachtechアイコンにリンクを追加

--- app/components/Molecules/header.tsx
+++ app/components/Molecules/header.tsx
@@ -1,22 +1,20 @@
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import { AppBar, Link, Toolbar, Typography } from "@mui/material";
 import Image from "next/image";
 
 const Header = () => {
   return (
     <AppBar position="static" sx={{ bgcolor: "#FFCDCD" }}>
       <Toolbar>
-        <Image
-          src="/peachtech.png"
-          width={50}
-          height={50}
-          alt="peachtechのアイコン"
-        />
+        <Link href="/">
+          <Image
+            src="/peachtech.png"
+            width={50}
+            height={50}
+            alt="peachtechのアイコン"
+          />
+        </Link>
         <Typography
-          m={{xs:0, sm:"0 auto"}}
+          m={{ xs: 0, sm: "0 auto" }}
           color={"black"}
           fontSize={{ xs: 15, md: 20 }}
           fontWeight={"bold"}
