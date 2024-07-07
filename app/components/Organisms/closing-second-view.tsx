00817daa565a4461b421e1c6baaac65ef3a4a4bf
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-07-07 17:27:59 +0900

    closing-viewのUIを変更　（画像の位置、文言、ボタン変更）

--- app/components/Organisms/closing-second-view.tsx
+++ app/components/Organisms/closing-second-view.tsx
@@ -1,49 +1,87 @@
 import { Typography, Box } from "@mui/material";
-import Image from "next/image";
 import DecorationBar from "../Atoms/decoration-bar";
-import CustomButton from "../Atoms/custom-button";
+import WebTutorialButton from "../Atoms/web-tutorial-button";
 
 const ClosingScondView = () => {
   return (
-    <Box bgcolor="#fff7f7" pb={10}>
-      <Box textAlign="center" alignContent="center" bgcolor="#fff7f7" pt={5}>
-        <Image
-          src="/peachtech.png"
-          width={150}
-          height={150}
-          alt="peachtechのアイコン"
-        />
-        <Typography fontWeight="bold" mt={-3}>
-          PeachTech
-        </Typography>
-      </Box>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      bgcolor="#fff7f7"
+      py={{ xs: 3, md: 4 }}
+      px={{ xs: 3, md: 0 }}
+    >
       <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        bgcolor="white"
-        mx={{ xs: 3, md: 50 }}
-        my={{ xs: 7, md: 10 }}
-        px={4}
-        py={4}
+        bgcolor={"white"}
+        boxShadow={"0.5px 1px 5px -2px gray"}
+        borderRadius={1}
+        px={{ xs: 3, md: 40 }}
+        py={{ xs: 8, md: 10 }}
       >
-        <Typography lineHeight={3} fontSize={{ xs: 13, md: 15 }}>
-          ・早くプログラミングを始めたい方
-          <br />
-          ・どんなことが学べるかいち早く知りたい方
+        <Typography
+          fontSize={{ xs: 13, md: 30 }}
+          fontWeight={"bold"}
+          borderBottom={"1px solid #CCCCCC"}
+        >
+          Webサイト,Webアプリ開発に興味がある方へ
         </Typography>
-      </Box>
-      <Box textAlign="center" alignContent="center">
-        <Typography my={{ xs: 1, md: 3 }}>こちらもおすすめ！</Typography>
+        <Box textAlign="center" alignContent="center">
+          <Typography
+            lineHeight={2}
+            fontSize={{ xs: 13, md: 16 }}
+            my={{ xs: 1, md: 3 }}
+            mt={{ xs: 2, md: 0 }}
+          >
+            Webプログラミングを実践的に学べるチュートリアルを用意しています！
+            <br />
+            PeachTechのプログラミング未経験者の多くがwebチュートリアルから始めています！
+          </Typography>
+          <Typography
+            mt={{ xs: 5, md: 10 }}
+            mb={5}
+            fontSize={{ xs: 13, md: 16 }}
+          >
+            以下のような方にもおすすめです！
+          </Typography>
+        </Box>
+        <Box
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          sx={{ flexFlow: "column" }}
+        >
+          <Box
+            bgcolor={"#F6F4F4"}
+            px={{ md: 5 }}
+            py={3}
+            width={{ xs: "100%", md: "50%" }}
+          >
+            <Typography ml={{ xs: 4, md: 5 }} fontSize={{ xs: 13, md: 16 }}>
+              ・早くプログラミングを学びたい人
+              <br />
+              ・実際になにか作ってみたい人
+              <br />
+              ・新しいことに挑戦したい人
+              <br />
+            </Typography>
+          </Box>
+          <Typography
+            mt={{ xs: 6, md: 8 }}
+            mb={4}
+            fontSize={{ xs: 13, md: 16 }}
+          >
+            興味のある方は下のボタンをクリック
+          </Typography>
+        </Box>
         <Box display="flex" justifyContent="center" alignItems="center">
           <DecorationBar transform="rotate(-30deg)" />
           <DecorationBar transform="rotate(0deg)" />
           <DecorationBar transform="rotate(30deg)" />
         </Box>
-        <CustomButton
-          text="Webチュートリアル"
-          href="https://github.com/yusei53/PeachTech-web-tutorial"
-        />
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <WebTutorialButton />
+        </Box>
       </Box>
     </Box>
   );
